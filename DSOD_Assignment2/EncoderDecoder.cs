38c6aa4c8f32b00b20dffc7d09196b83ab59224a
Vageesh Bhasin <vageesh_bhasin@outlook.com>
2014-09-27 16:32:19 -0700

    Added Encoder Decoder

---
+++ DSOD_Assignment2/EncoderDecoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSOD_Assignment2
+{
+    class EncoderDecoder
+    {
+        public static void Encode()
+        {
+
+        }
+        public static void Decode()
+        {
+
+        }
+    }
+}
